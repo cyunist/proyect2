18173143edd42ba1a0662c26d7ac61fc806e1667
Carlos Yunis <csyt@hotmail.com>
2023-03-24 21:58:57 -0500

    JavaScript agregar y consultar

--- js/codigo.js
+++ js/codigo.js
@@ -1,9 +1,34 @@
 
-guardar_localStorage();
+const nombre = document.getElementById("nombreEnt");
+const apellido = document.getElementById("apellidoEnt");
+const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+const cuerpoTabla = document.getElementById("cuerpoTabla");
 
-function guardar_localStorage(){
+/*
+const usuarios = localStorage.getItem("usuarios")|| [] ;
+console.log(usuarios);
 
-    let nombre = "carlos";
-    localStorage.setItem("nombre", nombre);
+const agregar = () => { 
+    console.log("agregarUsuaio");
 
 }
+*/
+
+const agregar = () => {
+  const usuario = {
+    id: crypto.randomUUID(),
+    nombre: nombre.value,
+    apellido: apellido.value,
+  };
+
+  usuarios.push(usuario);
+  localStorage.setItem("usuarios", JSON.stringify(usuarios));
+  mostrar();
+};
+
+
+// mostrar los usuarios//
+
+const mostrar = () => {   
+    console.log(usuarios);
+}; 
